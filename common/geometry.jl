f15692b2a4a1351e15479f3458afa69a1ef9eafb
Grotball <140941449+Grotball@users.noreply.github.com>
2024-09-25 11:30:59 +0800

    Update Geometry struct

--- common/geometry.jl
+++ common/geometry.jl
@@ -7,10 +7,12 @@ struct Square <: AbstractPrim end
 struct Circle <: AbstractPrim end
 
 # Only 2d shapes
-mutable struct Geometry{T<:AbstractFloat, M<:AbstractMatrix{T}}
-    pos::MVector{2, T}
-    transform::M
-    v::MVector{2, T}
+mutable struct Geometry{T<:AbstractFloat}
     prim::AbstractPrim
-    collide::Bool
+    transform::MMatrix{2, 2, T, 4}
+    pos::MVector{2, T}
+    v::MVector{2, T}
+    ω::T
+    isCollider::Bool
+    isSource::Bool
 end
