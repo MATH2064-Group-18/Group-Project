19a52e42e0f3b5de45b470b2274bc6c2a4c983fc
Grotball <140941449+Grotball@users.noreply.github.com>
2024-10-09 08:30:18 +0800

    Update Swirl.jl branch to add in setup.jl

--- setup.jl
+++ setup.jl
@@ -2,6 +2,6 @@ import Pkg
 
 Pkg.activate((@__DIR__))
 
-Pkg.add(url="https://github.com/MATH2064-Group-18/Swirl.jl")
+Pkg.add(url="https://github.com/MATH2064-Group-18/Swirl.jl", rev="release-0.2.1")
 
 Pkg.instantiate()
